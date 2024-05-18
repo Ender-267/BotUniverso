d45e176e0fe318a0ce3045077dcf35022611def1
Ender-267 <albertolozano2004@gmail.com>
2024-05-18 18:10:48 +0200

    filtardo de cosas que no sean rangos, ahora lee bien los ascensos

--- uniscraper.py
+++ uniscraper.py
@@ -10,17 +10,20 @@ URL = 'https://tienda.universocraft.com/'
 FICHERO = Path('./rangos.txt')
 
 class Entrada:
-    def __init__(self, usuario: str, rango: str, duracion: str):
+    def __init__(self, usuario: str, rango_anterior: str, rango_nuevo: str,duracion: str):
         self.usuario = usuario
-        self.rango = rango
+        self.rango_anterior = rango_anterior
+        self.rango_nuevo = rango_nuevo
         self.duracion = duracion
 
     def __eq__(self, other):
-        return (self.usuario == other.usuario) and (self.rango == other.rango) and (self.duracion == other.duracion)
+        return (self.usuario == other.usuario) and (self.rango_anterior == other.rango_anterior) and (self.rango_nuevo == other.rango_nuevo) and (self.duracion == other.duracion)
 
     def __str__(self):
         fecha = datetime.now().strftime("%Y/%m/%d %H:%M")
-        return f"{fecha} {self.usuario} {self.rango} {self.duracion}\n"
+        if self.rango_anterior:
+            return f"{fecha} {self.usuario} {self.rango_anterior} a {self.rango_nuevo} {self.duracion}\n"
+        return f"{fecha} {self.usuario} {self.rango_nuevo} {self.duracion}\n"
     
 
 
@@ -37,7 +40,10 @@ def scrape():
         s = i.replace('\n', '')
         s = s.split(' ')
         if i != 'NF\n':
-            contenido_fichero2.append(Entrada(s[2], s[3], s[4]))
+            if len(s) == 7:
+                contenido_fichero2.append(Entrada(s[2], s[3], s[5], s[6]))
+            else:
+                contenido_fichero2.append(Entrada(s[2], None, s[3], s[4]))
     contenido_fichero = contenido_fichero2
     
     try:
@@ -53,7 +59,8 @@ def scrape():
     encontrado = False
     for tag in lista_h6:
         s = parsear_string_de_rango(tag.get_text())
-        rangos.append(s)
+        if s:
+            rangos.append(s)
 
     rangos2 = []
     for i in rangos:
@@ -77,17 +84,28 @@ def scrape():
 
 
 def parsear_string_de_rango(s: str) -> Entrada:
+    s = s.rstrip()
+    s = s.replace('-', '') # Borrar guiones
     s = re.sub(r'\s+', ' ', s) # Borrar espacios duplicados
-    s = s.replace('\n', '') # Borrar saltos de linea
-    s = s.split(' ') # Dividir 
+    s = s.split(' ') # Dividir
+    lista_rangos = ('Júpiter', 'Neptuno', 'Mercurio', 'Saturno')
+    if '1' in s[2] and 'Mes' in s[3]:
+        s[2] = 'MENSUAL'
+        s.pop(3)
     username = s[0]
-    rango = s[2]
-    duracion = s[3]
+    if s[1] not in lista_rangos:
+        return None
+    if s[2].lower() == 'a':
+        rango_anterior = s[1]
+        rango_nuevo = s[3]
+        duracion = s[4]
+    else:
+        rango_anterior = None
+        rango_nuevo = s[1]
+        duracion = s[2]
     if 'PERMANENTE' in duracion:
         duracion = 'PERMANENTE'
-    elif '1' in duracion:
-        duracion = 'MENSUAL'
-    return Entrada(username, rango, duracion)
+    return Entrada(username, rango_anterior, rango_nuevo, duracion)
 
 def schedulef():
     schedule.every(1).minutes.do(scrape)
@@ -96,4 +114,4 @@ def schedulef():
         time.sleep(10)
 
 if __name__ == "__main__":
-    schedulef()
+    scrape()
