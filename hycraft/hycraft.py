58e2a5015156e7f2117df319cea3075a904927ef
Ender-267 <albertolozano2004@gmail.com>
2024-06-02 04:47:10 +0200

    fuck blok, all my homies hate blok

--- hycraft/hycraft.py
+++ hycraft/hycraft.py
@@ -75,8 +75,26 @@ def rewrite_data(data):
 def unistats(nick: str) -> bool:
     url = "https://stats.universocraft.com/jugador/" + nick
     headers = {
-        'User-Agent': 'BOT'
-    }
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
+        'Accept-Encoding': 'gzip, deflate, br, zstd',
+        'Accept-Language': 'es-ES,es;q=0.5',
+        'Cache-Control': 'max-age=0',
+        'Cookie': 'cf_clearance=OOZqPQtjmslqn9gTJxSA8MhoHPR8JJaJVgkLmq4uIZw-1717294885-1.0.1.1-LFoNtw5l1mlxXDgWB1_KBYlLkohLJl_iKqz31Um0ClOiFjnWF95uazzIHdKYimq.cKJIYlDUCOE73YBJuPKTbQ',
+        'Priority': 'u=0, i',
+        'Referer': 'https://stats.universocraft.com/',
+        'Sec-Ch-Ua': '"Brave";v="125", "Chromium";v="125", "Not.A/Brand";v="24"',
+        'Sec-Ch-Ua-Mobile': '?0',
+        'Sec-Ch-Ua-Model': '""',
+        'Sec-Ch-Ua-Platform': '"Linux"',
+        'Sec-Ch-Ua-Platform-Version': '"6.5.0"',
+        'Sec-Fetch-Dest': 'document',
+        'Sec-Fetch-Mode': 'navigate',
+        'Sec-Fetch-Site': 'same-origin',
+        'Sec-Fetch-User': '?1',
+        'Sec-Gpc': '1',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36'
+}
 
     session = requests.Session()
     session.headers.update(headers)
@@ -114,33 +132,35 @@ if __name__ == '__main__':
         print(obj)
     else:
         encontrado = True
-        with open(BASE_NUEVA2, 'r') as archivo:
-            data = archivo.readlines()
-            try:
-                for i in data:
-                    with open(FINAL, 'a') as final:
-                        linea = i.split()
-                        nick = linea[0]
-                        passw = linea[1]
-                        if encontrado == False:
-                            if nick.lower() == obj.lower():
-                                encontrado = True
-                            continue
-                        while True:
-                            r = unistats(nick)
-                            if r == "ERROR":
-                                sleep(5)
-                            else:
-                                break
-                            
-                        s = "{:<16} {:<16} {}\n".format(nick, passw, r)
-                        if r not in ('USU', 'ERROR', 'PREMIUM', 'NO ENCONTRADO'):
-                            final.write(s)
-                            s = "{:<16} {:<16} {}{}{}\n".format(nick, passw, Fore.GREEN, r, Style.RESET_ALL)
-                        else:
-                            s = "{:<16} {:<16} {}{}{}\n".format(nick, passw, Fore.RED, r, Style.RESET_ALL)
-                        print(s, end='')
-                        sleep(1.2)
-            except KeyboardInterrupt:
-                print(f"\n{nick}")
+    with open(BASE_NUEVA2, 'r') as archivo:
+        data = archivo.readlines()
+        
+
+    try:
+        for i in data:
+            with open(FINAL, 'a') as final:
+                linea = i.split()
+                nick = linea[0]
+                passw = linea[1]
+                if encontrado == False:
+                    if nick.lower() == obj.lower():
+                        encontrado = True
+                    continue
+                while True:
+                    r = unistats(nick)
+                    if r == "ERROR":
+                        sleep(5)
+                    else:
+                        break
+                    
+                s = "{:<16} {:<16} {}\n".format(nick, passw, r)
+                if r not in ('USU', 'ERROR', 'PREMIUM', 'NO ENCONTRADO'):
+                    final.write(s)
+                    s = "{:<16} {:<16} {}{}{}\n".format(nick, passw, Fore.GREEN, r, Style.RESET_ALL)
+                else:
+                    s = "{:<16} {:<16} {}{}{}\n".format(nick, passw, Fore.RED, r, Style.RESET_ALL)
+                print(s, end='')
+                sleep(1.2)
+    except KeyboardInterrupt:
+        print(f"\n{nick}")
 
