9a492ab5ebf50fecc5436aa2004f2293bab33581
100495914 <albertolozano2004@gmail.com>
2024-06-04 04:05:56 +0200

    no puedo mas

--- v2/scraper.py
+++ v2/scraper.py
@@ -1,41 +1,32 @@
 from bs4 import BeautifulSoup
 from time import sleep
 import sqlite3
-import requests
 import locale
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-import undetected_chromedriver as uc
+import urllib3
 
 BASE_DE_DATOS_SQL = './v2/base.db'
 TXT_HYCRAFT = './v2/hycraft.txt'
 
 def cf_captcha():
-    driver = webdriver.Chrome()
+    driver = webdriver.Firefox()
     driver.get("https://stats.universocraft.com/jugador/Ender267")
-    captcha_pasado = 0
-    sleep(1000)
-    while captcha_pasado != 2:
-        sleep(3)
+
+    iframe = driver.find_elements(By.TAG_NAME, "iframe")
+    if iframe:
+        driver.switch_to.frame(iframe[0])
+        inputs = driver.find_elements(By.TAG_NAME,"input")
+        for i in inputs:
+            atributo = i.get_attribute("type")
+            print(atributo)
+            if atributo == "checkbox":
+                break
+        captcha = i
+        captcha.click()
+        sleep(10)
         iframe = driver.find_elements(By.TAG_NAME, "iframe")
-        if iframe:
-            if captcha_pasado == 0:
-                driver.switch_to.frame(iframe[0])
-            captcha_pasado = 1
-            inputs = driver.find_elements(By.TAG_NAME,"input")
-            for i in inputs:
-                atributo = i.get_attribute("type")
-                print(atributo)
-                if atributo == "checkbox":
-                    break
-            captcha = i
-            captcha.click()
-            sleep(10)
-        iframe = driver.find_elements(By.TAG_NAME, "iframe")
-        if not iframe:
-            captcha_pasado = 2
+
 
     driver.refresh()
     alerta = driver.switch_to.alert
@@ -46,46 +37,42 @@ def cf_captcha():
 def unistats(nick: str, token: str) -> tuple | str:
     url = "https://stats.universocraft.com/jugador/" + nick
     headers = {
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-        "Accept-Encoding": "gzip, deflate, br, zstd",
-        "Accept-Language": "es-ES,es;q=0.8,en-US;q=0.5,en;q=0.3",
-        "Connection": "keep-alive",
-        "Content-Length": "5031",
-        "Content-Type": "application/x-www-form-urlencoded",
-        "Cookie": "cf_clearance=X3pOxWsENxU5yxKbv425cBdqFlAg59Nan4DaNLk9jb8-1717463427-1.0.1.1-slQEhvZ2T1iSC.V6S9z_O0_mzdT9CvqOLoLUBncviiJLNKQuiFge50jEzjIPqvqag4b_yifrx4kAd5A3c.dWQg",
-        "Host": "stats.universocraft.com",
-        "Origin": "https://stats.universocraft.com",
-        "Priority": "u=1",
-        "Referer": "https://stats.universocraft.com/",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+        "Accept-Encoding": "gzip, deflate, zstd",
+        "Accept-Language": "es-ES,es;q=0.9",
+        "Cache-Control": "max-age=0",
+        "Cookie": "cf_clearance=ODqtjvhD5W0W4r.A13oBF.p7FgNzZkaIvO_Bg8oZYnc-1717466413-1.0.1.1-61tXKKl4vO0NAI.x6NY_FlAEmXwukJNPSBc4yjYesq0c9yHYwcwTwZzbIgAvnsRKrcL_uFCgPmciQY3_sXZQ8w",
+        "Priority": "u=0, i",
+        "Sec-Ch-Ua": '"Not/A)Brand";v="8", "Chromium";v="126", "Brave";v="126"',
+        "Sec-Ch-Ua-Mobile": "?0",
+        "Sec-Ch-Ua-Model": '""',
+        "Sec-Ch-Ua-Platform": '"Windows"',
+        "Sec-Ch-Ua-Platform-Version": '"15.0.0"',
         "Sec-Fetch-Dest": "document",
         "Sec-Fetch-Mode": "navigate",
-        "Sec-Fetch-Site": "same-origin",
+        "Sec-Fetch-Site": "none",
         "Sec-Fetch-User": "?1",
-        "TE": "trailers",
+        "Sec-Gpc": "1",
         "Upgrade-Insecure-Requests": "1",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:126.0) Gecko/20100101 Firefox/126.0"
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"
     }
-
-
-
-
-
-    session = requests.Session()
-    session.headers.update(headers)
+    sesion = urllib3.PoolManager()
     try:
-        respuesta = session.get(url)
-        respuesta.raise_for_status() # Errores HTTP
-    except requests.RequestException as e:
+        respuesta = sesion.request('GET', url, headers=headers)
+        sleep(2)
+        respuesta = sesion.request('GET', url, headers=headers)
+    except urllib3.exceptions.HTTPError as e:
         print(f"Error fetching the webpage: {e}")
         return f'ERROR {e}'
 
+
     url_error = "https://stats.universocraft.com/?error=true"
-    if respuesta.url == url_error:
+    if respuesta.geturl() == url_error:
         return ('NO ENCONTRADO', None)
 
-    
-    pagina = BeautifulSoup(respuesta.text, 'html.parser')
-
+    print(respuesta.status)
+    pagina = BeautifulSoup(respuesta.data, 'html.parser')
+    print(pagina.prettify())
     tag_premium = pagina.find('span', class_="ProfileTag TagPremium")
     if tag_premium:
         premium = 'SI'
@@ -113,4 +100,4 @@ def scraper():
 
 
 if __name__ == '__main__':
-    token = cf_captcha()
+    unistats('Ender267', None)
